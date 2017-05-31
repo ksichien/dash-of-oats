8de3516dcf2f63a41bef5714a0243d0b2bd68ad6
maya-go <mayu666@gmail.com>
2017-05-31 22:23:34 +0200

    Added js file for recipe data

---
+++ public/javascripts/recipe_data.js
@@ -0,0 +1,27 @@
+var recipes = [
+  {
+    name: "pancake_recipe",
+    ingredients: {
+      "tbs white vinegar": 1,
+      "cups unsweetened almond milk": 7/8,
+      "cups rolled oats": 2,
+      "medium ripe banana": 1,
+      "tsp baking powder": 2,
+      "tsp baking soda": 1/2,
+      "tsp ground cinnamon": 1/2,
+      "tsp salt": 1/2,
+      "tbs maple syrup": 3,
+      "tsp vanilla extract": 2
+    }
+  },
+  {
+    name: "energybar_recipe",
+    ingredients: {
+      "cups rolled oats": 1,
+      "cups pitted oats": 1,
+      "cups shredded coconut": 1/4,
+      "cups of your chosen ingredients (dried fruit, nuts, seeds)": 1/2,
+      "tbs coconut oil": 1
+    }
+  }
+]
