e90279f7ee7b88a45d2183c5d5857f0bb7862a53
m-gb <maya.goldberg90@gmail.com>
2018-02-16 19:19:35 +0100

    refactor calculator

--- public/javascripts/recipe_calculator.js
+++ public/javascripts/recipe_calculator.js
@@ -118,17 +118,19 @@ const recipes = [
 
 $('input').change(function () {
   for (let i in recipes) {
-    const div = `#${recipes[i].name}`
-    const ul = document.createElement('ul')
-    const qty = document.getElementById('quantity').value
-    for (let key in recipes[i].ingredients) {
-      if (recipes[i].ingredients.hasOwnProperty(key)) {
-        const li = document.createElement('li')
-        const value = recipes[i].ingredients[key]
-        li.appendChild(document.createTextNode(`${value * qty}${key}`))
-        ul.appendChild(li)
+    if (document.getElementById(recipes[i].name)) {
+      const div = `#${recipes[i].name}`
+      const ul = document.createElement('ul')
+      const qty = document.getElementById('quantity').value
+      for (let key in recipes[i].ingredients) {
+        if (recipes[i].ingredients.hasOwnProperty(key)) {
+          const li = document.createElement('li')
+          const value = recipes[i].ingredients[key]
+          li.appendChild(document.createTextNode(`${value * qty}${key}`))
+          ul.appendChild(li)
+        }
       }
+      $(div).html(ul)
     }
-    $(div).html(ul)
   }
 })
